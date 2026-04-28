b16d452f617b0a034e6d4e979a795f803fd43961
sindrezoth <alex.sindre.gm@gmail.com>
2026-04-28 15:37:45 +0300

    will see

--- init.lua
+++ init.lua
@@ -1,3 +1,9 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
+vim.opt.winborder = "rounded"
+vim.opt.clipboard = "unnamedplus"
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 
@@ -9,7 +15,7 @@ vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.signcolumn = "yes"
 
-local servers = { "lua_ls", "pyright", "ts_ls" }
+local servers = { "lua_ls", "pyright", "ts_ls", "html", "cssls" }
 
 vim.pack.add({
   { src = "https://github.com/folke/tokyonight.nvim" },
@@ -19,6 +25,14 @@ vim.pack.add({
   { src = "https://github.com/nvim-mini/mini.nvim" },
   { src = "https://github.com/nvim-lua/plenary.nvim" },
   { src = "https://github.com/nvim-telescope/telescope.nvim" },
+  { src = "https://github.com/nvim-tree/nvim-tree.lua" },
+  { src = "https://github.com/saghen/blink.lib" },
+  { src = "https://github.com/saghen/blink.cmp" },
+  { src = "https://github.com/akinsho/toggleterm.nvim" },
+  { src = "https://github.com/folke/trouble.nvim" },
+  { src = "https://github.com/lewis6991/gitsigns.nvim" },
+  { src = "https://github.com/rafamadriz/friendly-snippets" },
+  { src = "https://github.com/L3MON4D3/LuaSnip" },
 });
 
 require("mason").setup()
@@ -28,15 +42,78 @@ require("mason-lspconfig").setup({
 })
 
 vim.lsp.config("lua_ls", {})
+vim.lsp.config("html", {
+  settings = {
+    html = {
+      autoClosingTags = true
+    }
+  }
+})
+vim.lsp.config("cssls", {})
 vim.lsp.config("ts_ls", {})
 vim.lsp.enable(servers)
 
 require("mini.pick").setup()
-require("mini.pairs").setup()
+require("mini.pairs").setup({
+  modes = { insert = true, command = false, terminal = false },
+  skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+  skip_ts = { "string" },
+
+  mappings = {
+    ["<"] = false,
+  },
+})
 require("mini.surround").setup()
 require("mini.statusline").setup()
 
--- require("telescope").setup()
+require("blink.cmp").setup({
+  fuzzy = { implementation = "lua" }, --"prefer_rust_with_warning" },
+  sources = {
+    default = { "lsp", "path", "snippets", "buffer" },
+    providers = {
+      lsp = {
+        score_offset = 100,
+      },
+      buffer = {
+        score_offset = -50,
+        keyword_length = 3, -- only show buffer after 5 chars
+      },
+      path = {
+        score_offset = -5,
+      },
+    },
+  },
+  keymap = {
+    ["<CR>"] = { "accept", "fallback"},
+    ["<C-k>"] = { "select_prev", "fallback"},
+    ["<C-j>"] = { "select_next", "fallback"},
+    ["<C-h>"] = { "hide", "fallback"},
+  }
+})
+
+local nvimTreeConfig = {
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+}
+
+require("nvim-tree").setup(nvimTreeConfig)
+
+require("toggleterm").setup({
+  open_mapping = [[<C-\>]],
+  direction = "float"
+})
+
+require("trouble").setup()
 
 local function pack_clean()
   local active_plugins = {}
@@ -67,12 +144,21 @@ vim.cmd.colorscheme("tokyonight-night")
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>jk", ":q<CR>")
-vim.keymap.set("n", "<leader>sh", ":vsplit<CR>")
-vim.keymap.set("n", "<leader>sl", ":bel vsplit<CR>:Telescope find_files<CR>")
-vim.keymap.set("n", "<leader>sj", ":bel split<CR>")
-vim.keymap.set("n", "<leader>sk", ":split<CR>")
+
+-- vim.keymap.set("n", "<leader>sh", ":vsplit<CR>:Telescope find_files<CR>")
+-- vim.keymap.set("n", "<leader>sl", ":bel vsplit<CR>:Telescope find_files<CR>")
+-- vim.keymap.set("n", "<leader>sj", ":bel split<CR>:Telescope find_files<CR>")
+-- vim.keymap.set("n", "<leader>sk", ":split<CR>:Telescope find_files<CR>")
+vim.keymap.set("n", "<leader>sh", ":vsplit<CR>:Pick files<CR>")
+vim.keymap.set("n", "<leader>sl", ":bel vsplit<CR>:Pick files<CR>")
+vim.keymap.set("n", "<leader>sj", ":bel split<CR>:Pick files<CR>")
+vim.keymap.set("n", "<leader>sk", ":split<CR>:Pick files<CR>")
+
 vim.keymap.set("n", "<leader>sm", ":res<CR>:vert res<CR>")
 vim.keymap.set("n", "<leader>s=", "<C-w>=")
+
+vim.keymap.set("n", "<space>ee", ":NvimTreeToggle<CR>")
+vim.keymap.set("n", "<space>er", ":NvimTreeRefresh<CR>")
 
 vim.keymap.set("n", "<leader>nh", ":set nohls<CR>")
 
@@ -81,9 +167,15 @@ vim.keymap.set("n", "<C-j>", "<C-w><C-j>")
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>")
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>")
 
+vim.keymap.set("n", "<Tab>", ":tabnext<CR>")
+vim.keymap.set("n", "<S-Tab>", ":tabprev<CR>")
+
+vim.keymap.set("n", "<leader>ff", ":Pick files<CR>")
+vim.keymap.set("n", "<leader>gg", ":Pick grep_live<CR>")
+
+vim.keymap.set("n", "<leader>fp", ":Pick files<CR>")
+
+vim.keymap.set("n", "<leader>gd", ":Trouble diagnostics toggle<CR>")
+
 vim.keymap.set("n", "<leader>pc", pack_clean)
 vim.keymap.set("n", "<leader>cc", ":checkhealth<CR>")
-
-
-vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>")
-vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>")
